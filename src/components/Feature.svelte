da9618c80928bcee4dcf04dd4edcdfe3c81a0c54
neha.tiwari <neha.tiwari@accionlabs.com>
2019-11-15 20:36:53 +0530

    Feature started

--- src/components/Feature.svelte
+++ src/components/Feature.svelte
@@ -6,6 +6,19 @@
   h2 {
     text-decoration:underline;
   }
+  .featureList {
+    text-align:center;
+  }
+  
 </style>
 
 <h2 class="center">Feature Component</h2>
+
+<ul class="featureList">
+      <li>Feature</li>
+      <li>New approach to building user interfaces</li>
+      <li>Write Less Code</li>
+      <li>No Virtual DOM</li>
+      <li>No Virtual DOM</li>
+      <li>Truely Reactive </li>
+</ul>
