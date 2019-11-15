732302dfaf9bdeffae83ad6e9ed58a9890d257a2
rupali777 <rupalibadgujar1234@gmail.com>
2019-11-15 19:03:39 +0530

    added header menu

--- src/components/DashboardHeader.svelte
+++ src/components/DashboardHeader.svelte
@@ -5,5 +5,14 @@
   
 </style>
 <header>
-  test 1 test 2
+ <div>
+   <img src="../../public/image/logo.png" alt="Logo">
+   <h3 style="float: left;">Evento</h3>
+    <ul>
+      <li>Feature</li>
+      <li>Why Evento?</li>
+      <li>Contact Us</li>
+      <li>login</li>
+    </ul>
+ </div>
 </header>
