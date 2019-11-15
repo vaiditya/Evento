0dec0fe897a426ac593f074330ecbc3e9c18a132
Tanvi Shah <tanvi.shah@accionlabs.com>
2019-11-15 20:09:35 +0530

    justified menu

--- src/components/DashboardHeader.svelte
+++ src/components/DashboardHeader.svelte
@@ -4,31 +4,40 @@
 <style lang="scss">
   header {
     display: flex;
-    background: rgba(0,0,0,0.8);
+    background: rgba(2, 8, 32, 0.8);
     color: #fff;
     padding: 10px 20px;
     display: flex;
     justify-content: space-between;
 }
+.logo{
+      display: flex;
+    justify-content: center;
+    align-items: center;
+}
 ul {
     display: flex;
-    list-style-type: none;
-    li{
-    padding:0 10px;
-  }
+    list-style-type: none;   
 }
+li{
+    padding: 0 10px;
+    a{
+      color:#fff;text-decoration:none;
+      &:hover{color:#ddd;}
+    }
+  }
 
 </style>
 <header>
    <div class="logo">
-   <img src="../../public/image/logo.png" alt="Logo">
+   <img src="/image/logo.png" alt="Logo">
    Evento
    </div>
     <ul>
-      <li>Feature</li>
-      <li>Why Evento?</li>
-      <li>Contact Us</li>
-      <li>Login</li>
+      <li><a href="/">Feature</a></li>
+      <li><a href="/">Why Evento?</a></li>
+      <li><a href="/">Contact Us</a></li>
+      <li><a href="/">Login</a></li>
     </ul>
  
 </header>
