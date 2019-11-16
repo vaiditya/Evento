b6c81ca1f0e37e3b4eaff7af95c7cc4809f22a8c
Vaiditya Chauhan <vaidityachauhan@gmail.com>
2019-11-16 10:38:10 +0530

    Feat(Loader):lOader added on all pages

--- src/containers/DashBoardContainer.svelte
+++ src/containers/DashBoardContainer.svelte
@@ -3,14 +3,27 @@
   import Feature from "./Feature.svelte";
   import About from "./About.svelte";
   import Contact from "./Contact.svelte";
+
+  let loader = true;
+  setTimeout(generateData, 4000);
+  async function generateData() {
+    loader = false;
+  }
 </script>
 
 <style>
+
 </style>
 
 <div>
-  <Slider />
-  <Feature />
-  <About />
-  <Contact />
+  {#if !loader}
+    <Slider />
+    <Feature />
+    <About />
+    <Contact />
+  {:else}
+    <img
+      src="https://i.pinimg.com/originals/39/ee/de/39eede5b8818d7c02d2340a53a652961.gif"
+      alt="" />
+  {/if}
 </div>
--- src/containers/EventContainer.svelte
+++ src/containers/EventContainer.svelte
@@ -1,6 +1,12 @@
 <script>
   import Event from "../components/Event.svelte";
 
+  let loader = true;
+  setTimeout(generateData, 4000);
+  async function generateData() {
+    loader = false;
+  }
+
   let data = [
     {
       name: "ACCATHON 2019",
@@ -34,8 +40,14 @@
 </style>
 
 <div>
-  {#each data as { name }, i}
-    <Event {name} />
-  {/each}
+  {#if !loader}
+    {#each data as { name }, i}
+      <Event {name} />
+    {/each}
+  {:else}
+    <img
+      src="https://i.pinimg.com/originals/39/ee/de/39eede5b8818d7c02d2340a53a652961.gif"
+      alt="" />
+  {/if}
 
 </div>
